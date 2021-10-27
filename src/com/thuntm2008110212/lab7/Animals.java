5e43142d0f2f37af05feea4c6e2e20032a167163
minhnguyen1105 <minhthutrannguyen1105@gmail.com>
2021-10-27 22:14:41 +0700

    bt tuan 7

---
+++ src/com/thuntm2008110212/lab7/Animals.java
@@ -0,0 +1,22 @@
+package com.thuntm2008110212.lab7;
+
+public class Animals {
+    public static void main(String[] args) {
+    protected String picture;
+    protected String food;
+    protected int hunger;
+    protected int location;
+
+    protected void makeNoise() {
+        System.out.println("Đang kêu");
+    }
+    protected void eat() {
+        System.out.println("Đang ăn");
+    }
+    protected void sleep() {
+        System.out.println("Đang ngủ");
+    }
+    protected void roam() {
+        System.out.println("Di chuyển");
+    }
+}
---
+++ src/com/thuntm2008110212/lab7/AnimalsTestDrive.java
@@ -0,0 +1,43 @@
+package com.thuntm2008110212.lab7;
+
+public class AnimalsTestDrive {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeNoise();
+        dog.eat();
+        dog.sleep();
+        dog.roam();
+
+        Wolf wolf = new Wolf();
+        wolf.makeNoise();
+        wolf.eat();
+        wolf.sleep();
+        wolf.roam(); 
+
+        Tiger tiger = new Tiger();
+        tiger.makeNoise();
+        tiger.eat();
+        tiger.sleep();
+        tiger.roam();
+
+        Cat cat = new Cat();
+        cat.makeNoise();
+        cat.eat();
+        cat.sleep();
+        cat.roam();
+
+
+        Hippo hippo = new Hippo();
+        hippo.makeNoise();
+        hippo.eat();
+        hippo.sleep();
+        hippo.roam();
+
+        Lion lion = new Lion();
+        lion.makeNoise();
+        lion.eat();
+        lion.sleep();
+        lion.roam();
+    }
+
+}
